fe6237a0b5484144274328ad4603a0b66e87a763
Zach Raines <raineszm@gmail.com>
2020-12-06 17:27:55 -0500

    refactor(rebber): create dedicated functions for stringify various nodes to latex

--- src/processor.ts
+++ src/processor.ts
@@ -7,6 +7,11 @@ import { wikiLinkPlugin } from 'remark-wiki-link';
 import frontmatter from 'remark-frontmatter';
 import rebber from 'rebber';
 import { Node } from 'unist';
+import { Parent } from 'mdast';
+
+const consume = (_ctx: unknown, _node: Node): string => '';
+const wikiLink = consume;
+const yaml = consume;
 
 export const markdownToTex = unified()
   .use(markdown)
@@ -19,14 +24,55 @@ export const markdownToTex = unified()
   })
   .use(rebber, {
     overrides: {
-      wikiLink: stringifyWikilink,
-      inlineMath: stringifyWikilink,
-      math: stringifyWikilink,
-      textDirective: stringifyWikilink,
-      yaml: stringifyWikilink,
+      wikiLink,
+      inlineMath,
+      textDirective,
+      yaml,
+      math: displayMath,
     },
   });
 
-function stringifyWikilink(ctx: unknown, node: Node): string {
-  return '';
+interface InlineMath extends Node {
+  type: 'inlineMath';
+  value: string;
+}
+
+interface Math extends Node {
+  type: 'math';
+  value: string;
+}
+
+function inlineMath(_ctx: unknown, node: Node): string {
+  const mathNode = node as InlineMath;
+  return `$${mathNode.value}$`;
+}
+
+function displayMath(_ctx: unknown, node: Node): string {
+  const mathNode = node as Math;
+  return `\\[\n${mathNode.value}\n\\]`;
+}
+
+interface TextDirective extends Parent {
+  type: 'textDirective';
+  name: string;
+  attributes?: unknown;
+}
+
+interface LabelDirective extends TextDirective {
+  name: 'label';
+  attributes: {
+    text: string;
+  };
+}
+
+function textDirective(_ctx: unknown, node: Node): string {
+  const directive = node as TextDirective;
+  if (directive.name === 'label') {
+    return stringifyLabel(directive as LabelDirective);
+  }
+  throw new Error(`Unsupported directive type: ${directive.name}`);
+}
+
+function stringifyLabel(directive: LabelDirective): string {
+  return `\\label{${directive.attributes.text}}`;
 }
