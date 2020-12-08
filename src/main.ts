30e7f3ac270e20649b990c84f35dd9e00eee1f77
Zach Raines <raineszm@gmail.com>
2020-12-07 19:36:45 -0500

    feature(settings): add default ref command to settings

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
-import { Plugin, PluginSettingTab, TFile } from 'obsidian';
+import {
+  App,
+  ButtonComponent,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  TFile,
+} from 'obsidian';
 import { TeXPrinter } from './texPrinter';
 import { remote } from 'electron';
 import * as fs from 'fs';
 import { promisify } from 'util';
+import { ExportToTexSettings } from './settings';
 
 export default class ExportToTeXPlugin extends Plugin {
-  public onload(): void {
-    console.log('loading plugin');
+  settings: ExportToTexSettings = new ExportToTexSettings();
+
+  public async onload(): Promise<void> {
+    const settings = await this.loadData();
+    if (settings !== null) {
+      this.settings = settings;
+    }
 
     this.addCommand({
       id: 'export-to-tex',
@@ -67,20 +80,47 @@ export default class ExportToTeXPlugin extends Plugin {
 }
 
 class ExportToTeXSettingTab extends PluginSettingTab {
+  constructor(app: App, readonly plugin: ExportToTeXPlugin) {
+    super(app, plugin);
+  }
+
   display(): void {
     const { containerEl } = this;
 
     containerEl.empty();
 
-    containerEl.createEl('h2', { text: 'Settings exporting to TeX' });
+    containerEl.createEl('h2', { text: 'Settings for exporting to TeX' });
 
-    // new Setting(containerEl)
-    // 	.setName('Setting #1')
-    // 	.setDesc('It\'s a secret')
-    // 	.addText(text => text.setPlaceholder('Enter your secret')
-    // 		.setValue('')
-    // 		.onChange((value) => {
-    // 			console.log('Secret: ' + value);
-    // 		}));
+    new Setting(containerEl)
+      .setName('Ref command')
+      .setDesc(
+        'Command to use when converting links to headings/blocks to refs.',
+      )
+      .addText((text) =>
+        text
+          .setValue(this.plugin.settings.refCommand)
+          .onChange(async (value) => {
+            this.plugin.settings.refCommand = value;
+            await this.plugin.saveData(this.plugin.settings);
+          }),
+      );
+
+    new ButtonComponent(containerEl)
+      .setButtonText('Reset to default')
+      .onClick(async () => {
+        await remote.dialog
+          .showMessageBox({
+            title: 'Reset settings to default?',
+            type: 'question',
+            message: 'Are you sure?',
+            buttons: ['No', 'Yes'],
+          })
+          .then(async (value) => {
+            if (value.response === 0) return;
+            this.plugin.settings = new ExportToTexSettings();
+            await this.plugin.saveData(this.plugin.settings);
+            this.display();
+          });
+      });
   }
 }
---
+++ src/settings.ts
@@ -0,0 +1,3 @@
+export class ExportToTexSettings {
+  refCommand: string = 'cref';
+}
