c4017f61c0c0944e4be99366a66af0da60f82c2b
Zach Raines <raineszm@gmail.com>
2020-12-14 14:05:25 -0500

    refactor(stringify): move stringifying functions to their own file

--- src/processor.ts
+++ src/processor.ts
@@ -6,29 +6,10 @@ import directive from 'remark-directive';
 import { wikiLinkPlugin } from 'remark-wiki-link';
 import frontmatter from 'remark-frontmatter';
 import rebber from 'rebber';
-import { Node } from 'unist';
 import { embed } from './embed';
-import { displayMath, inlineMath } from './math';
 import { labels } from './labels';
-import { AugmentedContext, getContext, OptionalContext } from './data';
-import {
-  assertHeading,
-  assertLabelDirective,
-  assertLabeledLink,
-  LabelDirective,
-} from './mdastInterfaces';
+import { rebberOverrides } from './stringify';
 
-const consume = (_ctx: unknown, _node: Node): string => '';
-const yaml = consume;
-
-const rebberOverrides = {
-  wikiLink: ensureContext(wikiLink),
-  inlineMath: ensureContext(inlineMath),
-  textDirective: ensureContext(textDirective),
-  yaml,
-  math: ensureContext(displayMath),
-  heading: ensureContext(heading),
-};
 export const markdownToTex = unified()
   .use(markdown)
   .use(gfm)
@@ -44,55 +25,3 @@ export const markdownToTex = unified()
     overrides: rebberOverrides,
   })
   .freeze();
-
-function ensureContext(
-  fn: (ctx: AugmentedContext, node: Node) => string,
-): (ctx: OptionalContext, node: Node) => string {
-  return (ctx, node) => {
-    return fn(getContext(ctx), node);
-  };
-}
-
-function textDirective(_ctx: AugmentedContext, node: Node): string {
-  assertLabelDirective(node);
-  return stringifyLabel(node);
-}
-
-function stringifyLabel(directive: LabelDirective): string {
-  return `\\label{${directive.data?.label ?? ''}}`;
-}
-
-function wikiLink(ctx: AugmentedContext, node: Node): string {
-  assertLabeledLink(node);
-  const {
-    exportToTex: { refCommand },
-  } = ctx;
-
-  const { alias, label } = node.data;
-  if (!node.value.contains('#') || label === undefined) {
-    return alias ?? node.value;
-  }
-  return `${alias ?? ''}\\${refCommand}{${label}}`;
-}
-
-const headingNames = [
-  'section',
-  'subsection',
-  'subsubsection',
-  'paragraph',
-  'subparagraph',
-];
-
-function heading(ctx: AugmentedContext, node: Node): string {
-  assertHeading(node);
-
-  if (node.depth > 5) {
-    return '';
-  }
-  const cmd = headingNames[node.depth - 1];
-  const text = node.children
-    .map((content) => rebber.toLaTeX(content, ctx))
-    .join('');
-  const label = node.data?.label as string;
-  return `\\${cmd}{${text}}\\label{${label}}`;
-}
---
+++ src/stringify.ts
@@ -0,0 +1,73 @@
+import { displayMath, inlineMath } from './math';
+import { Node } from 'unist';
+import { AugmentedContext, getContext, OptionalContext } from './data';
+import {
+  assertHeading,
+  assertLabelDirective,
+  assertLabeledLink,
+  LabelDirective,
+} from './mdastInterfaces';
+import rebber from 'rebber';
+
+const consume = (_ctx: unknown, _node: Node): string => '';
+const yaml = consume;
+export const rebberOverrides = {
+  wikiLink: ensureContext(wikiLink),
+  inlineMath: ensureContext(inlineMath),
+  textDirective: ensureContext(textDirective),
+  yaml,
+  math: ensureContext(displayMath),
+  heading: ensureContext(heading),
+};
+
+function ensureContext(
+  fn: (ctx: AugmentedContext, node: Node) => string,
+): (ctx: OptionalContext, node: Node) => string {
+  return (ctx, node) => {
+    return fn(getContext(ctx), node);
+  };
+}
+
+function textDirective(_ctx: AugmentedContext, node: Node): string {
+  assertLabelDirective(node);
+  return stringifyLabel(node);
+}
+
+function stringifyLabel(directive: LabelDirective): string {
+  return `\\label{${directive.data?.label ?? ''}}`;
+}
+
+function wikiLink(ctx: AugmentedContext, node: Node): string {
+  assertLabeledLink(node);
+  const {
+    exportToTex: { refCommand },
+  } = ctx;
+
+  const { alias, label } = node.data;
+  if (!node.value.contains('#') || label === undefined) {
+    return alias ?? node.value;
+  }
+  return `${alias ?? ''}\\${refCommand}{${label}}`;
+}
+
+const headingNames = [
+  'section',
+  'subsection',
+  'subsubsection',
+  'paragraph',
+  'subparagraph',
+];
+
+function heading(ctx: AugmentedContext, node: Node): string {
+  assertHeading(node);
+
+  if (node.depth > 5) {
+    return '';
+  }
+  const cmd = headingNames[node.depth - 1];
+  const text = node.children
+    .map((content) => rebber.toLaTeX(content, ctx))
+    .join('');
+  const label = node.data?.label as string;
+  return `\\${cmd}{${text}}\\label{${label}}`;
+}
