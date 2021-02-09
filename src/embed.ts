62787bcabeb8c23f864ee971cebde21a63ae93cc
Zach Raines <raineszm@gmail.com>
2021-02-09 11:24:13 -0500

    refactor(embed): refactor embed processing into a class in preparation of handling image embeds

--- src/embed.ts
+++ src/embed.ts
@@ -33,14 +33,17 @@ async function embedTransformer(
 
       if (parent === undefined)
         throw new Error('found an embed without a parent');
+
+      const resolver = new EmbedResolver(processor, file, node);
       promises.push(
-        resolveEmbed(processor, node, file)
+        resolver
+          .resolve()
           .then((newNode) => {
             parent.children[index] = newNode;
           })
           .catch((reason) => {
             file.message(reason, node, 'embed:error');
-            parent.children[index] = failedEmbed(file, node);
+            parent.children[index] = resolver.failedEmbed();
           }),
       );
     },
@@ -51,91 +54,110 @@ async function embedTransformer(
   });
 }
 
-function failedEmbed(parentFile: VFile, node: EmbedDirective): Node {
-  const embedTarget = node.attributes.target;
-  parentFile.message(`Failed to resolve embed ${embedTarget}`, node);
-  return { type: 'inlineCode', value: `Missing ${embedTarget}` };
-}
+class EmbedResolver {
+  constructor(
+    readonly processor: Processor,
+    readonly parentFile: VFile,
+    readonly node: EmbedDirective,
+  ) {}
 
-async function resolveEmbed(
-  processor: Processor,
-  node: EmbedDirective,
-  parentFile: VFile,
-): Promise<Node> {
-  const embedTarget = node.attributes.target;
-  parentFile.info(`Resolving embed "${embedTarget}"`, node);
-
-  const metadata = processor.data('metadata');
-  if (!(metadata instanceof MetadataCache)) {
-    throw Error(
-      'metadata must be passed to the processor in the form of an obsidian MetadataCache',
+  failedEmbed(): Node {
+    const embedTarget = this.node.attributes.target;
+    this.parentFile.message(
+      `Failed to resolve embed ${embedTarget}`,
+      this.node,
     );
+    return { type: 'inlineCode', value: `Missing ${embedTarget}` };
   }
 
-  const { file, result, subpath } = getTarget(node, parentFile, metadata);
+  async resolve(): Promise<Node> {
+    const embedTarget = this.node.attributes.target;
+    this.parentFile.info(`Resolving embed "${embedTarget}"`, this.node);
 
-  if (result === null || file === undefined) {
-    return failedEmbed(parentFile, node);
+    const metadata = this.processor.data('metadata');
+    if (!(metadata instanceof MetadataCache)) {
+      throw Error(
+        'metadata must be passed to the processor in the form of an obsidian MetadataCache',
+      );
+    }
+
+    const { file, result, subpath } = this.getTarget(metadata);
+
+    if (result === null || file === undefined) {
+      return this.failedEmbed();
+    }
+
+    this.parentFile.info(`Reading embedded file ${file.basename}`, this.node);
+    if (!(await file.vault.adapter.exists(file.path))) {
+      this.parentFile.message(`${file.basename} does not exists`, this.node);
+      return this.failedEmbed();
+    }
+    return await this.processMarkdownEmbed(embedTarget, subpath, file, result);
   }
 
-  parentFile.info(`Reading embedded file ${file.basename}`, node);
-  if (!(await file.vault.adapter.exists(file.path))) {
-    parentFile.message(`${file.basename} does not exists`, node);
-    return failedEmbed(parentFile, node);
+  async processMarkdownEmbed(
+    embedTarget: string,
+    subpath: string,
+    file: TFile,
+    result: HeadingSubpathResult | BlockSubpathResult,
+  ): Promise<Node> {
+    const fileData = await file.vault.cachedRead(file);
+
+    const data = fileData.slice(result.start.offset, result.end?.offset);
+
+    this.parentFile.info(`Parsing "${embedTarget}"`, this.node);
+
+    const embedFile = makeVFile(data, file.path, subpath);
+    const processed = this.processor.parse(embedFile);
+    this.parentFile.messages.push(...embedFile.messages);
+
+    return processed;
   }
 
-  const fileData = await file.vault.cachedRead(file);
-
-  const data = fileData.slice(result.start.offset, result.end?.offset);
-
-  parentFile.info(`Parsing "${embedTarget}"`, node);
-
-  const embedFile = makeVFile(data, file.path, subpath);
-  const processed = processor.parse(embedFile);
-  parentFile.messages.push(...embedFile.messages);
-
-  return processed;
-}
-
-const FAILED_TARGET = {
-  subpath: '',
-  result: null,
-};
-
-function getTarget(
-  node: EmbedDirective,
-  file: VFile,
-  metadata: MetadataCache,
-): {
-  file?: TFile;
-  result: HeadingSubpathResult | BlockSubpathResult | null;
-  subpath: string;
-} {
-  const embedTarget = node.attributes.target;
-  const { path, subpath } = parseLinktext(embedTarget);
-
-  if (file.path === undefined) {
-    file.message(
-      `cannot resolve target of link ${embedTarget} as the path of the embedding file is not available`,
-      node,
-    );
-    return FAILED_TARGET;
-  }
-
-  const target = metadata.getFirstLinkpathDest(path, file.path);
-  if (target === null) {
-    file.message(`cannot resolve target of link ${embedTarget}`, node);
-    return FAILED_TARGET;
-  }
-  const cache = metadata.getFileCache(target);
-  if (cache === null) {
-    file.message(`cannot access file cache of ${embedTarget}`, node);
-    return FAILED_TARGET;
-  }
-
-  return {
-    file: target,
-    subpath,
-    result: resolveSubpath(cache, subpath.trimEnd()),
+  static readonly FAILED_TARGET = {
+    subpath: '',
+    result: null,
   };
+
+  getTarget(
+    metadata: MetadataCache,
+  ): {
+    file?: TFile;
+    result: HeadingSubpathResult | BlockSubpathResult | null;
+    subpath: string;
+  } {
+    const embedTarget = this.node.attributes.target;
+    const { path, subpath } = parseLinktext(embedTarget);
+
+    if (this.parentFile.path === undefined) {
+      this.parentFile.message(
+        `cannot resolve target of link ${embedTarget} as the path of the embedding file is not available`,
+        this.node,
+      );
+      return EmbedResolver.FAILED_TARGET;
+    }
+
+    const target = metadata.getFirstLinkpathDest(path, this.parentFile.path);
+    if (target === null) {
+      this.parentFile.message(
+        `cannot resolve target of link ${embedTarget}`,
+        this.node,
+      );
+      return EmbedResolver.FAILED_TARGET;
+    }
+    const cache = metadata.getFileCache(target);
+    if (cache === null) {
+      this.parentFile.message(
+        `cannot access file cache of ${embedTarget}`,
+        this.node,
+      );
+      return EmbedResolver.FAILED_TARGET;
+    }
+
+    return {
+      file: target,
+      subpath,
+      result: resolveSubpath(cache, subpath.trimEnd()),
+    };
+  }
 }
